3ad5b401f8ff8467c91229e774d72bd8e6660bcc
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-01-06 16:42:26 +0100

    Texture

--- main.ts
+++ main.ts
@@ -9,25 +9,61 @@ assert(gl !== null);
 let program = init_shader_program(gl, {
     vs: `
     attribute vec4 vertex_position;
+    attribute vec2 tex_coord;
     uniform mat4 projection_matrix;
+    varying vec2 v_tex_coord;
     void main() {
+        // tex_coord;
         gl_Position = projection_matrix * vertex_position;
+        v_tex_coord = tex_coord;
     }`,
     fs: `
     precision mediump float;
     uniform vec4 color;
+    uniform sampler2D texture;
+    varying vec2 v_tex_coord;
     void main() {
-        gl_FragColor = color;
+        // gl_FragColor = color;
+        color;
+        // v_tex_coord;
+        // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        // gl_FragColor = vec4(v_tex_coord, 0.0, 1.0);
+        gl_FragColor = texture2D(texture, v_tex_coord);
     }`,
     uniforms: ['color', 'projection_matrix'],
-    attribs: ['vertex_position'],
+    attribs: ['vertex_position', 'tex_coord'],
 });
 
 let position_buffer = gl.createBuffer();
 assert(position_buffer !== null);
 gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-let positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+let data = [
+     2.0,  1.0, 0.0,   1.0, 1.0,
+    -2.0,  0.5, 0.0,   0.0, 1.0,
+     2.0, -1.0, 0.0,   1.0, 0.0,
+    -2.0, -1.0, 0.0,   0.0, 0.0,
+];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+
+var texture = gl.createTexture();
+assert(texture !== null);
+gl.bindTexture(gl.TEXTURE_2D, texture);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+let size = 128;
+let tex_data = [];
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        tex_data.push(Math.random() * 255, Math.random() * 255, Math.random() * 255, 255);
+    }
+}
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, size, size, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+    new Uint8Array(tex_data));
+gl.generateMipmap(gl.TEXTURE_2D);
+
+////////////////
 
 gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
 gl.vertexAttribPointer(
@@ -35,10 +71,19 @@ gl.vertexAttribPointer(
     2, // num_components
     gl.FLOAT, // type
     false, // normalize
-    0, // stride
+    4 * 5, // stride
     0, // offset
 );
 gl.enableVertexAttribArray(program.attribs.vertex_position);
+gl.vertexAttribPointer(
+    program.attribs.tex_coord,
+    2, // num_components
+    gl.FLOAT, // type
+    false, // normalize
+    4 * 5, // stride
+    4 * 3, // offset
+)
+gl.enableVertexAttribArray(program.attribs.tex_coord);
 
 gl.useProgram(program.program);
 gl.uniform4f(program.uniforms.color, 1.0, 1.0, 0.0, 1.0);
@@ -60,7 +105,7 @@ function draw() {
     let m2 = mat4.create();
     mat4.lookAt(m2, [0, 1, -6], [0, 0, 0], [0, 1, 0]);
     mat4.multiply(m, m, m2);
-    mat4.rotateY(m, m, t / 1000);
+    mat4.rotateY(m, m, t / 2000);
     gl.uniformMatrix4fv(program.uniforms.projection_matrix, false, m);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
