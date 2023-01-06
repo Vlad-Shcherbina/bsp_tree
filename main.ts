65adc1cfe4085f057df0c4d1bc74580b190f0c82
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-01-06 04:13:16 +0100

    lookAt, rotation animation

--- main.ts
+++ main.ts
@@ -3,11 +3,8 @@ import { init_shader_program } from './webgl_util.js';
 import { mat4 } from './vendor/gl-matrix.js';
 
 let canvas = document.getElementById('glcanvas') as HTMLCanvasElement;
-let gl = canvas.getContext('webgl');
+let gl = canvas.getContext('webgl')!;
 assert(gl !== null);
-
-gl.clearColor(0.0, 0.0, 0.2, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
 
 let program = init_shader_program(gl, {
     vs: `
@@ -29,7 +26,7 @@ let program = init_shader_program(gl, {
 let position_buffer = gl.createBuffer();
 assert(position_buffer !== null);
 gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-let positions = [0.9, 0.9, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+let positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
 gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
@@ -46,14 +43,28 @@ gl.enableVertexAttribArray(program.attribs.vertex_position);
 gl.useProgram(program.program);
 gl.uniform4f(program.uniforms.color, 1.0, 1.0, 0.0, 1.0);
 
-const fieldOfView = Math.PI / 4;
-const aspect = canvas.clientWidth / canvas.clientHeight;
-const zNear = 0.1;
-const zFar = 100.0;
-const m = mat4.create();
-console.log(m);
-mat4.perspective(m, fieldOfView, aspect, zNear, zFar);
-mat4.translate(m, m, [0, 0, -6]);
-gl.uniformMatrix4fv(program.uniforms.projection_matrix, false, m);
+let start = +new Date();
 
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+function draw() {
+    let t = +new Date() - start;
+
+    gl.clearColor(0.0, 0.0, 0.2, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const fieldOfView = Math.PI / 4;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const m = mat4.create();
+    mat4.perspective(m, fieldOfView, aspect, zNear, zFar);
+    let m2 = mat4.create();
+    mat4.lookAt(m2, [0, 1, -6], [0, 0, 0], [0, 1, 0]);
+    mat4.multiply(m, m, m2);
+    mat4.rotateY(m, m, t / 1000);
+    gl.uniformMatrix4fv(program.uniforms.projection_matrix, false, m);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    requestAnimationFrame(draw);
+}
+draw();
