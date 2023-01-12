e55c36518de00e6142309d4c2ef9bf602f9a333e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-01-12 19:54:29 +0100

    Add console.trace() in assert
    For some reason, without it stack traces are sometimes not shown,
    and I don't feel like investigating.

--- assert.ts
+++ assert.ts
@@ -1,5 +1,6 @@
 export default function assert(cond: boolean, message?: string): asserts cond {
     if (!cond) {
+        console.trace();
         console.assert(cond, message || 'assert failed');
         throw message || 'assert failed';
     }
